72dc5af0b637afc04ac5d2dafcd48cf3ae82e53c
baorv <roanvanbao@gmail.com>
2019-09-25 19:49:30 +0700

    Initialize project

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,24 +24,14 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+    <div className="page">
+      <div className="flex-fill">
+        <Header />
+        <div className="my-3 mt-8">
+          <div className="container">{children}</div>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
